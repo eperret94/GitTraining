9e7870307e6a3ef57614d1d4f11960f48c5effdc
eperret94 <73056120+eperret94@users.noreply.github.com>
2021-01-11 16:41:03 +0100

    Initial commit

---
+++ src/test/java/MavenTutorial/AppTest_2.java
@@ -0,0 +1,25 @@
+package MavenTutorial;
+
+import static org.testng.AssertJUnit.assertTrue;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest_2 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+
+    @Test
+    public void printHello()
+    {
+        System.out.println("Hello");
+    }
+}
